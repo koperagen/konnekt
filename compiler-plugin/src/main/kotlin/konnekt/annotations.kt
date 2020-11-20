6c038bb469cdc8b1c948ee5f692a30eb63ae1016
Nikita Klimenko <nikitaklimenko2000@gmail.com>
2020-11-20 22:30:03 +0300

    add annotations

--- compiler-plugin/src/main/kotlin/konnekt/annotations.kt
+++
@@ -1,7 +0,0 @@
-package konnekt
-
-@Target(AnnotationTarget.CLASS)
-annotation class Client
-
-@Target(AnnotationTarget.FUNCTION)
-annotation class GET
---
+++ prelude/src/commonMain/kotlin/annotations.kt
@@ -0,0 +1,74 @@
+package konnekt.prelude
+
+@Target(AnnotationTarget.CLASS)
+annotation class client
+
+@Target(AnnotationTarget.FUNCTION)
+annotation class GET(val value: String)
+
+@Target(AnnotationTarget.VALUE_PARAMETER)
+annotation class Path(val value: String, val encoded: Boolean = false)
+
+@Target(AnnotationTarget.VALUE_PARAMETER)
+annotation class Query(val value: String, val encoded: Boolean = false)
+
+@Target(AnnotationTarget.VALUE_PARAMETER)
+annotation class Body
+
+@Target(AnnotationTarget.VALUE_PARAMETER)
+annotation class QueryMap
+
+@Target(AnnotationTarget.FUNCTION)
+annotation class DELETE(val value: String)
+
+@Target(AnnotationTarget.VALUE_PARAMETER)
+annotation class Field(val key: String)
+
+@Target(AnnotationTarget.VALUE_PARAMETER)
+annotation class FieldMap
+
+@Target(AnnotationTarget.FUNCTION)
+annotation class FormUrlEncoded
+
+@Target(AnnotationTarget.FUNCTION)
+annotation class HEAD
+
+@Target(AnnotationTarget.FUNCTION)
+annotation class Header
+
+@Target(AnnotationTarget.FUNCTION)
+annotation class HeaderMap
+
+@Target(AnnotationTarget.FUNCTION)
+annotation class Headers(vararg val values: String)
+
+@Target(AnnotationTarget.FUNCTION)
+annotation class HTTP
+
+@Target(AnnotationTarget.FUNCTION)
+annotation class Multipart
+
+@Target(AnnotationTarget.FUNCTION)
+annotation class OPTIONS
+
+@Target(AnnotationTarget.VALUE_PARAMETER)
+annotation class Part
+
+@Target(AnnotationTarget.VALUE_PARAMETER)
+annotation class PartMap
+
+@Target(AnnotationTarget.FUNCTION)
+annotation class PATCH
+
+@Target(AnnotationTarget.FUNCTION)
+annotation class POST
+
+@Target(AnnotationTarget.FUNCTION)
+annotation class PUT
+
+
+//
+annotation class Tag
+
+annotation class Url
+
