f08b74283d908ba5fddb786f137b2dc7ef2d65e1
Nikita Klimenko <nikitaklimenko2000@gmail.com>
2020-11-22 03:43:07 +0300

    Extract parsing functions

--- compiler-plugin/src/main/kotlin/GetPlugin.kt
+++ compiler-plugin/src/main/kotlin/GetPlugin.kt
@@ -143,47 +143,59 @@ fun toHeaderAnnotation(annotationEntry: KtAnnotationEntry): HeaderAnnotation {
 
 fun toSourceAnnotationOrNull(annotationEntry: KtAnnotationEntry, annotationName: String): SourceAnnotation? {
 
-  fun List<KtValueArgument>.values(): Pair<String, Boolean?> =
-      (getStringOrNull(0) ?: error("Expected first string param in argument list")) to
-          getBooleanOrNull(1)
-
   return when (annotationName) {
-    "Path" -> {
-      val args = annotationEntry.valueArgumentList?.arguments
-          ?: error("Argument list for path should contain 'id' and optoinally 'encoded'")
-      val (value, encoded) = args.values()
-      if (encoded != null) {
-        Path(value, encoded)
-      } else {
-        Path(value)
-      }
-    }
-    "Body" -> Body
-    "Query" -> {
-      val args = requireNotNull(annotationEntry.valueArgumentList?.arguments)
-      val (key, encoded) = args.values()
-      if (encoded != null) {
-        Query(key, encoded)
-      } else {
-        Query(key)
-      }
-    }
-    "Part" -> {
-      val args = requireNotNull(annotationEntry.valueArgumentList?.arguments)
-      val (key, encoded) = args.values()
-      if (encoded != null) {
-        Part(key, encoded)
-      } else {
-        Part(key)
-      }
-    }
-    "Field" -> {
-      val args = requireNotNull(annotationEntry.valueArgumentList?.arguments)
-      val key = requireNotNull(args.getStringOrNull(0))
-      Field(key)
-    }
+    "Path" -> path(annotationEntry)
+    "Body" -> body(annotationEntry)
+    "Query" -> query(annotationEntry)
+    "Part" -> part(annotationEntry)
+    "Field" -> field(annotationEntry)
     else -> null
   }
+}
+
+private fun List<KtValueArgument>.values(): Pair<String, Boolean?> =
+    (getStringOrNull(0) ?: error("Expected first string param in argument list")) to
+        getBooleanOrNull(1)
+
+private fun path(annotationEntry: KtAnnotationEntry): Path {
+    val args = annotationEntry.valueArgumentList?.arguments
+        ?: error("Argument list for path should contain 'id' and optoinally 'encoded'")
+    val (value, encoded) = args.values()
+    return if (encoded != null) {
+        Path(value, encoded)
+    } else {
+        Path(value)
+    }
+}
+
+private fun body(annotationEntry: KtAnnotationEntry): Body {
+    return Body
+}
+
+private fun query(annotationEntry: KtAnnotationEntry): Query {
+    val args = requireNotNull(annotationEntry.valueArgumentList?.arguments)
+    val (key, encoded) = args.values()
+    return if (encoded != null) {
+        Query(key, encoded)
+    } else {
+        Query(key)
+    }
+}
+
+private fun part(annotationEntry: KtAnnotationEntry): Part {
+    val args = requireNotNull(annotationEntry.valueArgumentList?.arguments)
+    val (key, encoded) = args.values()
+    return if (encoded != null) {
+        Part(key, encoded)
+    } else {
+        Part(key)
+    }
+}
+
+private fun field(annotationEntry: KtAnnotationEntry): Field {
+    val args = requireNotNull(annotationEntry.valueArgumentList?.arguments)
+    val key = requireNotNull(args.getStringOrNull(0))
+    return Field(key)
 }
 
 private fun List<KtValueArgument>.getStringOrNull(position: Int) =
