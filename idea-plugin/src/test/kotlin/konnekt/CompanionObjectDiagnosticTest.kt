7dbb5894b7703b9ba07749fc3a455360912f6b40
Nikita Klimenko <nikitaklimenko2000@gmail.com>
2021-02-01 21:15:47 +0300

    Test highlighting

---
+++ idea-plugin/src/test/kotlin/konnekt/CompanionObjectDiagnosticTest.kt
@@ -0,0 +1,42 @@
+package konnekt
+
+import arrow.meta.ide.IdeMetaPlugin
+import arrow.meta.ide.testing.IdeTest
+import arrow.meta.ide.testing.env.IdeTestSetUp
+import arrow.meta.ide.testing.env.ideTest
+import com.intellij.lang.annotation.HighlightSeverity
+import org.junit.Test
+
+object CompanionObjectInspectionCode {
+  val interfaceDeclaration = """
+    package test
+    
+    @Target(AnnotationTarget.CLASS)
+    annotation class client
+    
+    @client interface Test 
+  """.trimIndent()
+}
+
+class CompanionObjectDiagnosticTest : IdeTestSetUp() {
+
+  @Test
+  fun `companion object check for annotated interface`() =
+    ideTest(
+      myFixture = myFixture,
+      ctx = IdeMetaPlugin()
+    ) {
+      listOf(
+        IdeTest(
+          code = CompanionObjectInspectionCode.interfaceDeclaration,
+          test = { code, myFixture, ctx ->
+            collectInspections(code, myFixture, inspections = listOf(ctx.companionObjectInspection))
+          },
+          result = resolvesWhen("") { result ->
+            val highlight = result.firstOrNull { it.description == "Test".noCompanion }
+            highlight != null && highlight.severity == HighlightSeverity.ERROR
+          }
+        )
+      )
+    }
+}
