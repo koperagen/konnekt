29888c4829167f0b99a53a812b70d9d1ff85aa4c
Nikita Klimenko <nikitaklimenko2000@gmail.com>
2021-02-07 20:41:46 +0300

    Apply project code style

--- prelude/build.gradle.kts
+++ prelude/build.gradle.kts
@@ -1,34 +1,34 @@
 plugins {
-    kotlin("multiplatform")
+  kotlin("multiplatform")
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    mavenCentral()
-    jcenter()
+  mavenCentral()
+  jcenter()
 }
 
 kotlin {
-    /* Targets configuration omitted. 
-    *  To find out how to configure the targets, please follow the link:
-    *  https://kotlinlang.org/docs/reference/building-mpp-with-gradle.html#setting-up-targets */
-    jvm()
+  /* Targets configuration omitted.
+  *  To find out how to configure the targets, please follow the link:
+  *  https://kotlinlang.org/docs/reference/building-mpp-with-gradle.html#setting-up-targets */
+  jvm()
 
-    sourceSets {
+  sourceSets {
 
-        val commonMain by getting {
-            dependencies {
-                implementation(kotlin("stdlib"))
-            }
-        }
-        val commonTest by getting {
-            dependencies {
-                implementation(kotlin("test"))
-            }
-        }
-
+    val commonMain by getting {
+      dependencies {
+        implementation(kotlin("stdlib"))
+      }
     }
+    val commonTest by getting {
+      dependencies {
+        implementation(kotlin("test"))
+      }
+    }
+
+  }
 
 }
