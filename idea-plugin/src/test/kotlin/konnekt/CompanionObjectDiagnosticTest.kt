eb141a96e65b14d2ebe44c2a0763043106399743
Nikita Klimenko <nikitaklimenko2000@gmail.com>
2021-02-18 15:25:10 +0300

    Revert "Add missing import"
    This reverts commit 8093abb2

--- idea-plugin/src/test/kotlin/konnekt/CompanionObjectDiagnosticTest.kt
+++ idea-plugin/src/test/kotlin/konnekt/CompanionObjectDiagnosticTest.kt
@@ -6,7 +6,6 @@ import arrow.meta.ide.testing.dsl.IdeTestSyntax
 import arrow.meta.ide.testing.env.IdeTestSetUp
 import arrow.meta.ide.testing.env.ideTest
 import arrow.meta.ide.testing.Source
-import arrow.meta.plugins.optics.internals.noCompanion
 import com.intellij.codeInspection.InspectionProfileEntry
 import com.intellij.lang.annotation.HighlightSeverity
 import com.intellij.testFramework.fixtures.CodeInsightTestFixture
