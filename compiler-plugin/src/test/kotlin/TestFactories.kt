647c3fad7bc5a78d7380f4b58e0492d78e56005b
Nikita Klimenko <nikitaklimenko2000@gmail.com>
2021-04-11 21:41:27 +0300

    Create test factory in config scope

--- compiler-plugin/src/test/kotlin/TestFactories.kt
+++ compiler-plugin/src/test/kotlin/TestFactories.kt
@@ -1,8 +1,27 @@
 import arrow.meta.plugin.testing.Assert
 import arrow.meta.plugin.testing.CompilerTest
+import arrow.meta.plugin.testing.Config
 import arrow.meta.plugin.testing.assertThis
 import io.kotest.core.spec.style.scopes.DescribeScope
 import java.lang.reflect.InvocationTargetException
+
+interface WithCompilerTestConfig {
+  val config: CompilerTest.Companion.() -> List<Config>
+
+  suspend fun DescribeScope.it(name: String, expectOn: CompilerTest.Companion.() -> Assert, code: () -> String) =
+      it(name, expectOn, config, code)
+}
+
+suspend fun DescribeScope.it(name: String, expectOn: CompilerTest.Companion.() -> Assert, config: CompilerTest.Companion.() -> List<Config>, code: () -> String) {
+  it(name) {
+    try {
+      assertThis(CompilerTest(config, { code().source }, expectOn))
+    } catch (e: InvocationTargetException) {
+      // Message and stacktrace of exception could be too obscure
+      throw e.cause ?: throw e
+    }
+  }
+}
 
 suspend fun DescribeScope.it(name: String, expectOn: CompilerTest.Companion.() -> Assert, code: () -> String) {
   it(name) {
