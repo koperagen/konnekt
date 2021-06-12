dc6a38c299e3bd71f9f33422228993f6930358e4
Nikita Klimenko <nikitaklimenko2000@gmail.com>
2021-06-12 19:52:16 +0300

    Ensure that project without gradle plugin won't compile

--- gradle-plugin/src/test/kotlin/konnekt/gradle/KonnektPluginTest.kt
+++ gradle-plugin/src/test/kotlin/konnekt/gradle/KonnektPluginTest.kt
@@ -1,11 +1,13 @@
 package konnekt.gradle
 
 import io.kotest.assertions.asClue
+import io.kotest.assertions.throwables.shouldThrowExactly
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
 import org.gradle.testkit.runner.BuildResult
 import org.gradle.testkit.runner.GradleRunner
 import org.gradle.testkit.runner.TaskOutcome
+import org.gradle.testkit.runner.UnexpectedBuildFailure
 
 class KonnektPluginTest : FreeSpec({
 
@@ -17,7 +19,7 @@ class KonnektPluginTest : FreeSpec({
     withPluginClasspath()
   }
 
-  "simple plugin runs" {
+  "!simple plugin runs" {
     val result = GradleRunner {
       defaultConfig()
       buildFile.writeText(
@@ -48,7 +50,7 @@ class KonnektPluginTest : FreeSpec({
     }
   }
 
-  "sample project builds" {
+  "!sample project builds" {
     val result = GradleRunner {
       defaultConfig()
       buildFile.writeText("""
@@ -75,7 +77,7 @@ class KonnektPluginTest : FreeSpec({
     }
   }
 
-  "project with plugin" {
+  "project with plugin compiles" {
     val result = GradleRunner {
       defaultConfig()
       val src = projectDir.resolve("src").let {
@@ -127,6 +129,67 @@ class KonnektPluginTest : FreeSpec({
 
     result.asClue {
       it.task(":build")?.outcome shouldBe TaskOutcome.SUCCESS
+    }
+  }
+
+  "project without plugin failes" {
+    shouldThrowExactly<UnexpectedBuildFailure> {
+      val result = GradleRunner {
+        defaultConfig()
+        val src = projectDir.resolve("src").let {
+          it.mkdir()
+          it.resolve("main").let {
+            it.mkdir()
+            it.resolve("kotlin").let {
+              it.mkdir()
+              it.resolve("Main.kt")
+            }
+          }
+        }
+
+        src.writeText(
+            """
+        |import konnekt.prelude.Client
+        |import io.ktor.client.HttpClient
+        |
+        |@Client
+        |interface Test {
+        |
+        |   companion object
+        |}
+        |
+        |val api = Test(HttpClient())
+        """.trimMargin()
+        )
+        buildFile.writeText(
+            """
+        |plugins {
+        |    kotlin("jvm") version "1.4.10"
+        |}
+        |
+        |group = "org.example"
+        |version = "1.0-SNAPSHOT"
+        |
+        |repositories {
+        |    mavenLocal()
+        |    mavenCentral()
+        |}
+        |
+        |dependencies {
+        |    implementation(kotlin("stdlib"))
+        |    implementation("io.github.koperagen:prelude-jvm:1.4.10-SNAPSHOT")
+        |}
+        """.trimMargin()
+        )
+
+        withArguments("compileKotlin")
+        withDebug(true)
+        withGradleVersion("6.3")
+      }
+
+      result.asClue {
+        it.task(":compileKotlin")?.outcome shouldBe TaskOutcome.FAILED
+      }
     }
   }
 })
